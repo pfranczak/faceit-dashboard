3ea858f9c89c328e64ca4d67805b11505fd0a1cb
Jan Ignasik <ignasikj@gmail.com>
2020-04-18 11:22:44 +0200

    remove testing value

--- src/components/PlayerSearch.vue
+++ src/components/PlayerSearch.vue
@@ -23,7 +23,7 @@
         components: { PlayerCard },
         data() {
             return {
-                nickname: 'chaubee',
+                nickname: '',
                 cards: [],
             }
         },
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './views/Home';
 import LoginSite from './views/LoginSite';
-<<<<<<< HEAD
-import store from './store';
-=======
 import PlayerSite from './views/Player';
 import { globalState } from './main'
-
->>>>>>> c1c3783... Implement player page.
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
@@ -20,12 +15,7 @@ export const router = new VueRouter({
         { path: '/login' , component: LoginSite },
         { path: '/player/:id', name: 'player' , component: PlayerSite, props: true },
         { path: '/' , component: Home,  beforeEnter: (to, from, next) => {
-<<<<<<< HEAD
-            store.commit('authenticate', window.FACEIT.getAuthenticationStatus() === "connected");
-            console.log(window.FACEIT.getAuthenticationStatus() === "connected");
-=======
             globalState.authenticated = window.FACEIT.getAuthenticationStatus() === "connected"
->>>>>>> c1c3783... Implement player page.
             next()
         }},
     ]
